7f12bd4283eb1ebc04da91f051be9679803734a0
Luiz Paduan <luiz.paduanit@gmail.com>
2021-04-20 01:27:52 -0300

    refatoração e aplicação de middleware

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,23 @@ Route::get('/', function () {
 });
 
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::middleware(['auth'])->group(function() {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
+    Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
